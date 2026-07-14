e2aeb2229efe6b4dea76e42cf8a36d834ba2786e
Chirag Moolya <151938800+ChiragMoolya13@users.noreply.github.com>
2026-07-14 20:09:56 +0530

    Add: sort-colors - submission-9

---
+++ Data Structures & Algorithms/sort-colors/submission-9.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public void sortColors(int[] nums) {
+        int m = 0;
+        int i = 0;
+        int j = nums.length -1;
+        while(m<=j){
+            if(nums[m]==0){
+                swap(nums,m,i);
+                m++;
+                i++;
+            }
+            else if(nums[m]==1){
+                m++;
+            }
+            else{
+                swap(nums,m,j);
+                j--;
+            }
+        }
+    }
+}
